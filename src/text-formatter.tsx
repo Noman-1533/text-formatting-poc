67c5efb30e61451f89fee5b3677cfa30730e89d4
Noman-1533 <abu.noman@brainstation-23.com>
2025-01-28 15:51:21 +0600

    build

--- src/text-formatter.tsx
+++ src/text-formatter.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 // import "./TextFormatter.css";
